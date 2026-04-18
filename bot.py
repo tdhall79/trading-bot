87d676ae14b417e07413560741a9168c0a3adb09
tdhall79 <tdhall79@gmail.com>
2026-04-18 15:55:55 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,8 +6,7 @@ app = Flask(__name__)
 # === KRAKEN KEYS ===
 api = ccxt.kraken({
     'apiKey': 'nloJM+TPJGCYdu5+KobDyFDAd+DPZGCuHv7+CI1wu9bsOpUilxssMuKB',
-    'secret': 'vWzTX6FWytEDTn2t8wqHi1KIqb3JY/WkpEYzdbfOunzuS9wM8ALqOa9XlpPI4cnfav9iClQkTyI7i3fqJXNLsA==
-',
+    'secret': 'vWzTX6FWytEDTn2t8wqHi1KIqb3JY/WkpEYzdbfOunzuS9wM8ALqOa9XlpPI4cnfav9iClQkTyI7i3fqJXNLsA==',
     'enableRateLimit': True
 })
 
